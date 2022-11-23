0768b8d9fd9cda8d2af7a7c20762903ed84278e3
Jonathan Moore <jonm@duck.com>
2022-11-23 14:59:57 -0600

    Config file setup

--- SCPReplacer/Config.cs
+++ SCPReplacer/Config.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Exiled.API.Interfaces;
 
 namespace SCPReplacer
 {
-    public class Class1
+    public class Config : IConfig
     {
+        public bool IsEnabled { get; set; } = true;
     }
 }
